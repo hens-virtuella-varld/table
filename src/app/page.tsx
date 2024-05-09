3abda08db632471dd629cbb6e036c81ecfe4c506
Regan Ya <hens.virtuella.varld@gmail.com>
2024-05-09 20:27:04 +0200

    feat: add ThemeTogglerIcon and its button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ProductRow } from './components/ProductRow';
+import { ThemeTogglerIcon } from './components/ThemeTogglerIcon';
 
 import data from './data.json';
 
@@ -8,7 +9,13 @@ export default function Home() {
   const products = data.products;
 
   return (
-    <main className='p-5 flex md:justify-center'>
+    <main className='p-5 flex md:justify-center dark:bg-black'>
+      <button
+        className='bg-gray-400 bg-opacity-30 fixed right-6 bottom-6 hover:bg-opacity-50  active:bg-gray-500'
+        aria-label='Theme Toggler'
+      >
+        {<ThemeTogglerIcon className={'w-10 h-10'} />}
+      </button>
       <table className='text-left'>
         <thead>
           <tr>
