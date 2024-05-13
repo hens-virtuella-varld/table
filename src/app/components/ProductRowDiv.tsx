74ac7c3952ba5a09b4648843dc73214f43d44096
Regan Ya <hens.virtuella.varld@gmail.com>
2024-05-13 23:39:49 +0200

    fix: product column style

--- src/app/components/ProductRowDiv.tsx
+++ src/app/components/ProductRowDiv.tsx
@@ -13,7 +13,7 @@ export const ProductRowDiv = ({
 }: ProductRowProps) => {
   return (
     <div className='grid grid-cols-12 gap-2 items-center text-center border-b-2 border-solid border-gray-400 hover:bg-gray-200 dark:hover:bg-gray-800 group h-full'>
-      <div className='col-span-2 sticky left-0  p-2  bg-white dark:bg-gray-900 group-hover:bg-gray-200 group-hover:dark:bg-gray-800 shadow-lg-r dark:shadow-gray-700/50 h-full'>
+      <div className='col-span-2 sticky left-0 flex items-center justify-center  p-2 bg-white dark:bg-gray-900 group-hover:bg-gray-200 group-hover:dark:bg-gray-800 shadow-lg-r dark:shadow-gray-700/50 h-full'>
         {title}
       </div>
       <div>{Math.round((price * (100 - discount)) / 100)}</div>
